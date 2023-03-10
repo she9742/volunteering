5b8203ec9adb0d44884f3082226e37863926b231
Son Hyeeun <guswlsrkdmf@naver.com>
2023-03-10 16:04:54 +0900

    [refactor]redis 관련 추가 설정

--- src/test/java/com/team8/volunteerworkproject/VolunteerWorkProjectApplicationTests.java
+++ src/test/java/com/team8/volunteerworkproject/VolunteerWorkProjectApplicationTests.java
@@ -1,13 +1,9 @@
 package com.team8.volunteerworkproject;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class VolunteerWorkProjectApplicationTests {
-
-  @Test
-  void contextLoads() {
-  }
 
 }
