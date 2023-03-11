7f47d5842f4629554cc4096b3a41f1ae331c03c4
Son Hyeeun <guswlsrkdmf@naver.com>
2023-03-11 21:36:24 +0900

    [refactor]댓글에 아이디 대신 닉네임 가져오려고 시도

--- src/main/java/com/team8/volunteerworkproject/dto/response/ChallengeAuthCommentResponseDto.java
+++ src/main/java/com/team8/volunteerworkproject/dto/response/ChallengeAuthCommentResponseDto.java
@@ -14,6 +14,7 @@ public class ChallengeAuthCommentResponseDto {
     private Long challengeAuthId;
     private String comment;
     private String userId; //일단 유저 아이디 받아오고 리팩토링할 때 닉넥임으로 변경 예정
+    private String nickname;
 
 //    private List<ChallengeAuthComment> comments;
 
@@ -23,5 +24,6 @@ public class ChallengeAuthCommentResponseDto {
         this.challengeAuthId = challengeAuthComments.getChallengeAuth().getChallengeAuthId();
         this.comment = challengeAuthComments.getComment();
         this.userId = challengeAuthComments.getUserId();
+        this.nickname = getNickname();
     }
 }
